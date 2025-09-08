e3e44cafd638e434f8f9e02200d5a53bfe798cd5
MilanMareels <milan.mareels@gmail.com>
2025-09-08 15:09:34 +0200

    seo xclearing waiting for tekst

--- src/data/serviceItems.ts
+++ src/data/serviceItems.ts
@@ -7,8 +7,8 @@ export const serviceItems = [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     price: 120,
-    SEODescription: "Moderniseer je website zonder inhoudsverlies: fris design, snellere laadtijden én betere gebruiksvriendelijkheid. Perfect voor ondernemers!",
-    keywords: "website vernieuwen Merksem, modern webdesign Merksem, website optimalisatie, bestaande website updaten",
+    SEODescription: "",
+    keywords: "",
   },
   {
     id: 2,
@@ -18,8 +18,8 @@ export const serviceItems = [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     price: 60,
-    SEODescription: "Moderniseer je website zonder inhoudsverlies: fris design, snellere laadtijden én betere gebruiksvriendelijkheid. Perfect voor ondernemers!",
-    keywords: "website vernieuwen Merksem, modern webdesign Merksem, website optimalisatie, bestaande website updaten",
+    SEODescription: "",
+    keywords: "",
   },
   {
     id: 3,
@@ -29,40 +29,7 @@ export const serviceItems = [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     price: 140,
-    SEODescription: "Moderniseer je website zonder inhoudsverlies: fris design, snellere laadtijden én betere gebruiksvriendelijkheid. Perfect voor ondernemers!",
-    keywords: "website vernieuwen Merksem, modern webdesign Merksem, website optimalisatie, bestaande website updaten",
-  },
-  {
-    id: 4,
-    name: "servie-4",
-    src: ["https://dummyimage.com/600x400/000/fff", "https://flowbite.com/docs/images/carousel/carousel-1.svg", "https://flowbite.com/docs/images/carousel/carousel-1.svg"],
-    title: "Lorem ipsum",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    price: 100,
-    SEODescription: "Moderniseer je website zonder inhoudsverlies: fris design, snellere laadtijden én betere gebruiksvriendelijkheid. Perfect voor ondernemers!",
-    keywords: "website vernieuwen Merksem, modern webdesign Merksem, website optimalisatie, bestaande website updaten",
-  },
-  {
-    id: 5,
-    name: "servie-5",
-    src: ["https://dummyimage.com/600x400/000/fff", "https://flowbite.com/docs/images/carousel/carousel-1.svg", "https://flowbite.com/docs/images/carousel/carousel-1.svg"],
-    title: "Lorem ipsum",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    price: 20,
-    SEODescription: "Moderniseer je website zonder inhoudsverlies: fris design, snellere laadtijden én betere gebruiksvriendelijkheid. Perfect voor ondernemers!",
-    keywords: "website vernieuwen Merksem, modern webdesign Merksem, website optimalisatie, bestaande website updaten",
-  },
-  {
-    id: 6,
-    name: "servie-6",
-    src: ["https://dummyimage.com/600x400/000/fff", "https://flowbite.com/docs/images/carousel/carousel-1.svg", "https://flowbite.com/docs/images/carousel/carousel-1.svg"],
-    title: "Lorem ipsum",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    price: 60,
-    SEODescription: "Moderniseer je website zonder inhoudsverlies: fris design, snellere laadtijden én betere gebruiksvriendelijkheid. Perfect voor ondernemers!",
-    keywords: "website vernieuwen Merksem, modern webdesign Merksem, website optimalisatie, bestaande website updaten",
+    SEODescription: "",
+    keywords: "",
   },
 ];
